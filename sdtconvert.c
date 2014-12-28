bf3cf3855b6b758e7b63a825447570d140ddbf64
Mark Johnston <markjdb@gmail.com>
2014-12-27 23:19:52 -0800

    Progress towards recording probe instances in a linker set.
    Add a verbose mode.

--- sdtconvert.c
+++ sdtconvert.c
@@ -27,9 +27,12 @@
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
 
+#include <sys/queue.h>
+
 #include <assert.h>
 #include <err.h>
 #include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -38,19 +41,38 @@ __FBSDID("$FreeBSD$");
 #include <gelf.h>
 #include <libelf.h>
 
+#define	ELF_ERR()	(elf_errmsg(elf_errno()))
+#define	LOG(...) do {					\
+	if (verbose)					\
+		warnx(__VA_ARGS__);			\
+} while (0)
+
 #define	AMD64_CALL	0xe8
 #define	AMD64_JMP32	0xe9
 #define	AMD64_NOP	0x90
 #define	AMD64_RETQ	0xc3
 
+static const char probe_prefix[] = "__dtrace_probe_";
+
+static bool verbose = false;
+
+struct probe_instance {
+	const char	*symname;
+	uint64_t	offset;
+	SLIST_ENTRY(probe_instance) next;
+};
+
+SLIST_HEAD(probe_list, probe_instance);
+
+static Elf_Scn	*add_section(Elf *, const char *);
 static GElf_Sym	*symlookup(Elf_Data *, int);
 static int	process_rel(Elf *, GElf_Ehdr *, GElf_Shdr *, Elf_Data *,
-		    uint8_t *, GElf_Addr, GElf_Xword *);
-static void	process_reloc_scn(Elf *, GElf_Ehdr *, GElf_Shdr *, Elf_Scn *);
+		    uint8_t *, GElf_Addr, GElf_Xword *, struct probe_list *);
+static void	process_reloc_scn(Elf *, GElf_Ehdr *, GElf_Shdr *, Elf_Scn *,
+		    struct probe_list *);
 static void	process_obj(const char *);
+static void	record_instance(Elf *, Elf_Scn *, struct probe_instance *);
 static void	usage(void);
-
-static const char probe_prefix[] = "__dtrace_probe_";
 
 static GElf_Sym *
 symlookup(Elf_Data *symtab, int ndx)
@@ -62,11 +84,12 @@ symlookup(Elf_Data *symtab, int ndx)
 }
 
 /* XXX need obj filename for better error messages. */
-/* XXX need to compose consecutive relocations. */
 static int
 process_rel(Elf *e, GElf_Ehdr *ehdr, GElf_Shdr *symhdr, Elf_Data *symtab,
-    uint8_t *target, GElf_Addr offset, GElf_Xword *info)
+    uint8_t *target, GElf_Addr offset, GElf_Xword *info,
+    struct probe_list *plist)
 {
+	struct probe_instance *inst;
 	GElf_Sym *sym;
 	char *symname;
 	uint8_t opc;
@@ -74,7 +97,7 @@ process_rel(Elf *e, GElf_Ehdr *ehdr, GElf_Shdr *symhdr, Elf_Data *symtab,
 	sym = symlookup(symtab, GELF_R_SYM(*info));
 	symname = elf_strptr(e, symhdr->sh_link, sym->st_name);
 	if (symname == NULL)
-		errx(1, "elf_strptr: %s", elf_errmsg(elf_errno()));
+		errx(1, "elf_strptr: %s", ELF_ERR());
 
 	if (strncmp(symname, probe_prefix, sizeof(probe_prefix) - 1) != 0)
 		/* We're not interested in this relocation. */
@@ -92,17 +115,15 @@ process_rel(Elf *e, GElf_Ehdr *ehdr, GElf_Shdr *symhdr, Elf_Data *symtab,
 
 	switch (ehdr->e_machine) {
 	case EM_X86_64:
-		/* XXX sanity check on the relocation type? */
-#if 0
-		switch (GELF_R_TYPE(info)) {
+		switch (GELF_R_TYPE(*info)) {
 		case R_X86_64_64:
 		case R_X86_64_PC32:
 			break;
 		default:
-			errx(1, "unhandled relocation type %lu for %s",
-			    GELF_R_TYPE(info), symname);
+			errx(1, "unexpected relocation type %lu for %s",
+			    GELF_R_TYPE(*info), symname);
 		}
-#endif
+
 		/* Sanity checks. */
 		opc = target[offset - 1];
 		if (opc != AMD64_CALL && opc != AMD64_JMP32)
@@ -133,80 +154,149 @@ process_rel(Elf *e, GElf_Ehdr *ehdr, GElf_Shdr *symhdr, Elf_Data *symtab,
 		errx(1, "unhandled machine type 0x%x", ehdr->e_machine);
 	}
 
+	LOG("updated relocation for %s at 0x%lx", symname, offset - 1);
+
+	inst = malloc(sizeof(*inst));
+	if (inst == NULL)
+		err(1, "malloc");
+	inst->symname = symname;
+	inst->offset = offset;
+	SLIST_INSERT_HEAD(plist, inst, next);
+
 	return (0);
 }
 
 static void
-process_reloc_scn(Elf *e, GElf_Ehdr *ehdr, GElf_Shdr *shdr, Elf_Scn *scn)
+process_reloc_scn(Elf *e, GElf_Ehdr *ehdr, GElf_Shdr *shdr, Elf_Scn *scn,
+    struct probe_list *plist)
 {
 	GElf_Shdr symhdr;
 	GElf_Rel rel;
 	GElf_Rela rela;
-	Elf_Data *data, *symdata, *targdata;
+	Elf_Data *reldata, *symdata, *targdata;
 	Elf_Scn *symscn, *targ;
 	u_int i;
 	int ret;
 
 	targ = elf_getscn(e, shdr->sh_info);
 	if (targ == NULL)
-		errx(1, "failed to look up relocation section: %s",
-		    elf_errmsg(elf_errno()));
-	/* XXX multiple descriptors */
+		errx(1, "failed to look up relocation section: %s", ELF_ERR());
 	targdata = elf_getdata(targ, NULL);
 	if (targdata == NULL)
-		errx(1, "failed to look up target section data: %s",
-		    elf_errmsg(elf_errno()));
+		errx(1, "failed to look up target section data: %s", ELF_ERR());
 
 	symscn = elf_getscn(e, shdr->sh_link);
 	if (symscn == NULL)
-		errx(1, "failed to look up symbol table: %s",
-		    elf_errmsg(elf_errno()));
+		errx(1, "failed to look up symbol table: %s", ELF_ERR());
 	if (gelf_getshdr(symscn, &symhdr) == NULL)
-		errx(1, "failed to look up symbol table header: %s",
-		    elf_errmsg(elf_errno()));
+		errx(1, "failed to look up symbol table header: %s", ELF_ERR());
 
-	/* XXX handle multiple data descriptors */
 	symdata = elf_getdata(symscn, NULL);
 	if (symdata == NULL)
-		errx(1, "elf_getdata: %s", elf_errmsg(elf_errno()));
+		errx(1, "elf_getdata: %s", ELF_ERR());
 
 	i = 0;
-	data = NULL;
-	while ((data = elf_getdata(scn, data)) != NULL) {
+	reldata = NULL;
+	while ((reldata = elf_getdata(scn, reldata)) != NULL) {
 		for (; i < shdr->sh_size / shdr->sh_entsize; i++) {
 			if (shdr->sh_type == SHT_REL) {
-				if (gelf_getrel(data, i, &rel) == NULL)
-					errx(1, "gelf_getrel: %s",
-					    elf_errmsg(elf_errno()));
+				if (gelf_getrel(reldata, i, &rel) == NULL)
+					errx(1, "gelf_getrel: %s", ELF_ERR());
 				ret = process_rel(e, ehdr, &symhdr, symdata,
-				    targdata->d_buf, rel.r_offset, &rel.r_info);
+				    targdata->d_buf, rel.r_offset, &rel.r_info,
+				    plist);
 				if (ret == 0 &&
-				    gelf_update_rel(data, i, &rel) == 0)
+				    gelf_update_rel(reldata, i, &rel) == 0)
 					errx(1, "gelf_update_rel: %s",
-					    elf_errmsg(elf_errno()));
+					    ELF_ERR());
 			} else {
 				assert(shdr->sh_type == SHT_RELA);
-				if (gelf_getrela(data, i, &rela) == NULL)
-					errx(1, "gelf_getrela: %s",
-					    elf_errmsg(elf_errno()));
+				if (gelf_getrela(reldata, i, &rela) == NULL)
+					errx(1, "gelf_getrela: %s", ELF_ERR());
 				ret = process_rel(e, ehdr, &symhdr, symdata,
 				    targdata->d_buf, rela.r_offset,
-				    &rela.r_info);
+				    &rela.r_info, plist);
 				if (ret == 0 &&
-				    gelf_update_rela(data, i, &rela) == 0)
+				    gelf_update_rela(reldata, i, &rela) == 0)
 					errx(1, "gelf_update_rela: %s",
-					    elf_errmsg(elf_errno()));
+					    ELF_ERR());
+			}
+
+			/*
+			 * We've updated the relocation and the corresponding
+			 * text section.
+			 */
+			if (ret == 0) {
+				if (elf_flagdata(targdata, ELF_C_SET,
+				    ELF_F_DIRTY) == 0)
+					errx(1, "elf_flagdata: %s", ELF_ERR());
+				if (elf_flagdata(reldata, ELF_C_SET,
+				    ELF_F_DIRTY) == 0)
+					errx(1, "elf_flagdata: %s", ELF_ERR());
 			}
 		}
 	}
 }
 
+static Elf_Scn *
+add_section(Elf *e, const char *name)
+{
+	GElf_Shdr newshdr, strshdr;
+	Elf_Data *strdata, *newstrdata;
+	Elf_Scn *newscn, *strscn;
+	size_t len, shdrstrndx;
+
+	/* First add the section name to the section header string table. */
+	if (elf_getshdrstrndx(e, &shdrstrndx) != 0)
+		errx(1, "elf_getshdrstrndx: %s", ELF_ERR());
+	if ((strscn = elf_getscn(e, shdrstrndx)) == NULL)
+		errx(1, "elf_getscn on shdrstrtab: %s", ELF_ERR());
+	if (gelf_getshdr(strscn, &strshdr) != &strshdr)
+		errx(1, "gelf_getshdr on shdrstrtab: %s", ELF_ERR());
+	if ((strdata = elf_getdata(strscn, NULL)) == NULL)
+		errx(1, "elf_getdata on shdrstrtab: %s", ELF_ERR());
+	if ((newstrdata = elf_newdata(strscn)) == NULL)
+		errx(1, "elf_newdata on shdrstrtab: %s", ELF_ERR());
+
+	len = strlen(name) + 1;
+	newstrdata->d_align = strdata->d_align;
+	if ((newstrdata->d_buf = malloc(len)) == NULL)
+		err(1, "malloc");
+	memcpy(newstrdata->d_buf, name, len);
+	newstrdata->d_size = len;
+	newstrdata->d_type = strdata->d_type;
+	newstrdata->d_version = strdata->d_version;
+
+	strshdr.sh_size += len;
+
+	if ((newscn = elf_newscn(e)) == NULL)
+		errx(1, "elf_newscn: %s", ELF_ERR());
+	if (gelf_getshdr(newscn, &newshdr) != &newshdr)
+		errx(1, "gelf_getshdr: %s", ELF_ERR());
+
+	newshdr.sh_name = strshdr.sh_size - len;
+	newshdr.sh_type = SHT_PROGBITS;
+	newshdr.sh_flags = SHF_ALLOC;
+	newshdr.sh_addralign = 8;
+
+	if (gelf_update_shdr(newscn, &newshdr) == 0)
+		errx(1, "gelf_update_shdr: %s", ELF_ERR());
+	if (gelf_update_shdr(strscn, &strshdr) == 0)
+		errx(1, "gelf_update_shdr: %s", ELF_ERR());
+
+	LOG("added section %s", name);
+
+	return (newscn);
+}
+
 static void
 process_obj(const char *obj)
 {
+	struct probe_list plist;
 	GElf_Ehdr ehdr;
 	GElf_Shdr shdr;
-	Elf_Scn *scn;
+	struct probe_instance *inst, *tmp;
+	Elf_Scn *scn, *instscn;
 	Elf *e;
 	int fd;
 
@@ -216,42 +306,72 @@ process_obj(const char *obj)
 
 	e = elf_begin(fd, ELF_C_RDWR, NULL);
 	if (e == NULL)
-		errx(1, "elf_begin: %s", elf_errmsg(elf_errno()));
+		errx(1, "elf_begin: %s", ELF_ERR());
 
 	if (gelf_getehdr(e, &ehdr) == NULL)
-		errx(1, "gelf_getehdr: %s", elf_errmsg(elf_errno()));
+		errx(1, "gelf_getehdr: %s", ELF_ERR());
 	if (ehdr.e_type != ET_REL) {
 		warnx("invalid ELF type for '%s'", obj);
 		return;
 	}
 
+	SLIST_INIT(&plist);
+
+	/* Perform relocations for DTrace probe stub calls. */
 	scn = NULL;
 	while ((scn = elf_nextscn(e, scn)) != NULL) {
 		if (gelf_getshdr(scn, &shdr) == NULL)
-			errx(1, "gelf_getshdr: %s", elf_errmsg(elf_errno()));
+			errx(1, "gelf_getshdr: %s", ELF_ERR());
 
 		if (shdr.sh_type == SHT_REL || shdr.sh_type == SHT_RELA)
-			process_reloc_scn(e, &ehdr, &shdr, scn);
+			process_reloc_scn(e, &ehdr, &shdr, scn, &plist);
+	}
+
+	/* No SDT probes in this object file. */
+	if (SLIST_EMPTY(&plist)) {
+		LOG("no probes found in %s", obj);
+		return;
+	}
+
+	instscn = add_section(e, "set_sdt_instance_set");
+
+	SLIST_FOREACH_SAFE(inst, &plist, next, tmp) {
+		record_instance(e, instscn, inst);
+		SLIST_REMOVE(&plist, inst, probe_instance, next);
+		free(inst);
 	}
 
 	if (elf_update(e, ELF_C_WRITE) == -1)
-		errx(1, "elf_update: %s", elf_errmsg(elf_errno()));
+		errx(1, "elf_update: %s", ELF_ERR());
 
 	(void)elf_end(e);
 	(void)close(fd);
 }
 
 static void
+record_instance(Elf *e, Elf_Scn *instscn, struct probe_instance *inst)
+{
+	(void)e;
+	(void)instscn;
+	(void)inst;
+}
+
+static void
 usage(void)
 {
 
-	fprintf(stderr, "%s: <obj> [<obj> ...]\n", getprogname());
+	fprintf(stderr, "%s: [-v] <obj> [<obj> ...]\n", getprogname());
 	exit(1);
 }
 
 int
 main(int argc, char **argv)
 {
+
+	if (argc > 1 && strcmp(argv[1], "-v") == 0) {
+		verbose = true;
+		argv++; argc--;
+	}
 
 	if (argc <= 1)
 		usage();
