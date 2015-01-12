7e686ec782ab307adafa3e5452b1bf1fed11bcf5
Mark Johnston <markjdb@gmail.com>
2015-01-11 17:48:53 -0800

    Copyright 2015.

--- sdtconvert.c
+++ sdtconvert.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2014, 2015 Mark Johnston <markj@FreeBSD.org>
+ * Copyright (c) 2015 Mark Johnston <markj@FreeBSD.org>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
